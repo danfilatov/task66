10e972851159b7cf80feeeac13a818c34abc46f9
Denis Filatov <gooden@mail.ru>
2023-03-29 18:09:03 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите M:");
+int numM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N:");
+int numN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numM, int numN, int sum)
+{
+    if (numM > numN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от {numM} до {numN}: {sum}"); 
+        return;
+    }
+    sum = sum + (numM++);
+    GapNumberSum(numM, numN, sum);
+}
+
+GapNumberSum(numM, numN, 0);
